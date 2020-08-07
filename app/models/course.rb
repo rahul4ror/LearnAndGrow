af65370b2b84d7bafa3b4524828a2a6bb88c01c2
rahul4ror <rahul4ror@gmail.com>
2020-08-07 19:48:09 +0530

    Added navbar and flash messages

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,10 @@
 class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 5 }
+  has_rich_text :description
 
   belongs_to :user
 
-  has_rich_text :description
   def to_s
     title
   end
