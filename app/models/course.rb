4ef028c8e7597546e723ebfa1c713edee476dbad
rahul4ror <rahul4ror@gmail.com>
2020-08-07 19:25:45 +0530

    Added user to the course

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,10 @@ class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 5 }
 
+  belongs_to :user
+
   has_rich_text :description
+  def to_s
+    title
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+user = User.create!(email: "admin@example.com", password: "123456", password_confirmation: "123456")
+
 30.times do
   Course.create!([{
     title: Faker::Book.title,
-    description: Faker::Books::Lovecraft.sentence
+    description: Faker::Books::Lovecraft.sentence,
+    user: user
   }])
 end
